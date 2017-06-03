d47ddc28cddb574157fd2fd5ee7d41a0bbe60cda
xueeinstein <xueeinstein@gmail.com>
2017-06-03 19:48:26 +0800

    python tool to convert blob proto image mean to npy

---
+++ tools/convert_protomean.py
@@ -0,0 +1,17 @@
+import numpy as np
+import sys
+
+import caffe
+
+
+if len(sys.argv) != 3:
+    print('Usage: python convert_protomean.py proto.mean out.npy')
+    sys.exit()
+
+
+blob = caffe.proto.caffe_pb2.BlobProto()
+data = open(sys.argv[1], 'rb').read()
+blob.ParseFromString(data)
+arr = np.array(caffe.io.blobproto_to_array(blob))
+out = arr[0]
+np.save(sys.argv[2], out)
