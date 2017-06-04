c40b213e7d93cc2d358c3920f64d737d5b0ce0bd
xueeinstein <xueeinstein@gmail.com>
2017-06-04 09:34:37 +0800

    generate image set feature mat

---
+++ examples/shoes7k/generate_feature_mat.py
@@ -0,0 +1,83 @@
+'''
+Generate feature matrix
+
+Prepare files for image retrieval:
+image_files.npy
+fc7_features.npy
+latent_features.npy
+'''
+import os
+import sys
+import numpy as np
+
+from convert_shoes7k_data import get_images
+
+sys.path.append('../..')
+import config
+from layer_features import layer_features
+from retrieve import binary_hash_codes
+
+SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
+
+
+def generate_feature_matrix(model_file, deploy_file, imagemean_file):
+    """generate feature matrix of image dataset
+    save the matrix as npy file"""
+    # get image files
+    pos_images = get_images(config.eg_shoes7k_pos_path)
+    neg_images = get_images(config.eg_shoes7k_neg_path)
+
+    image_files = np.concatenate((pos_images, neg_images))
+    np.random.shuffle(image_files)
+
+    # feed the network and get feature vectors
+    feature_mat = {'fc7': [], 'latent': []}
+
+    batch = []
+    batch_size = 0
+    for image in image_files:
+        batch.append(image)
+        batch_size += 1
+
+        if batch_size == 1000:
+            for layer, mat in layer_features(feature_mat.keys(), model_file,
+                                             deploy_file, imagemean_file,
+                                             batch):
+                if layer == 'latent':
+                    mat = binary_hash_codes(mat)
+
+                feature_mat[layer].extend(mat)
+
+            batch = []
+            batch_size = 0
+
+    if batch_size > 0:
+        for layer, mat in layer_features(feature_mat.keys(), model_file,
+                                         deploy_file, imagemean_file, batch):
+            if layer == 'latent':
+                mat = binary_hash_codes(mat)
+
+            feature_mat[layer].extend(mat)
+
+    # save to npy files
+    np.save(os.path.join(SCRIPT_PATH, 'image_files.npy'), image_files)
+    for layer in feature_mat.keys():
+        npy_file = os.path.join(SCRIPT_PATH, layer + '_features.npy')
+        np.save(npy_file, np.array(feature_mat[layer]))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        usage = 'Usage: python generate_feature_mat.py' + \
+                ' model_file deploy_file imagemean_file'
+        print(usage)
+    else:
+        model_file = sys.argv[1]
+        deploy_file = sys.argv[2]
+        imagemean_file = sys.argv[3]
+
+        is_exists = os.path.exists(model_file) and os.path.exists(deploy_file)\
+            and os.path.exists(imagemean_file)
+
+        if is_exists:
+            generate_feature_matrix(model_file, deploy_file, imagemean_file)
